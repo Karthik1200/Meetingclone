9cb46ee78e02faa76711456c2f4da985533ebf9b
Karthik B <karthikbabuk1208@gmail.com>
2025-12-20 17:24:00 +0530

    add on the file

---
+++ meetclone/src/main/java/com/project/meetclone/MeetcloneApplication.java
@@ -0,0 +1,13 @@
+package com.project.meetclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetcloneApplication.class, args);
+	}
+
+}
